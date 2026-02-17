b31318042cfaa5d82875da961d5bddff8dc40932
유선영\catsp <catspie92@gmail.com>
2026-02-17 13:44:16 +0900

    feat. axois.ts 파일에 서버환경변수 적용

--- agency/frontend/src/api/axios.ts
+++ agency/frontend/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8082/api',
+    //baseURL: 'http://localhost:8082/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 5000,
     withCredentials: true,
 });
